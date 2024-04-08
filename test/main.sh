111c3be17f7e3a3bf9b813682123741bdb22fcfd
dmotte <37443982+dmotte@users.noreply.github.com>
2024-04-09 01:55:49 +0200

    Removed some unnecessary backslashes in Bash code

--- test/main.sh
+++ test/main.sh
@@ -6,8 +6,8 @@ cd "$(dirname "$0")"
 
 rm -f fake-notifier-status.txt
 
-bash fake-tail.sh | \
-    cargo run -q -- -di1 -m10 -- bash fake-notifier.sh 2>&1 | \
+bash fake-tail.sh |
+    cargo run -q -- -di1 -m10 -- bash fake-notifier.sh 2>&1 |
     tee output.txt
 
 diff {expected,output}.txt
