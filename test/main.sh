2f248a26aaba3242a5af761d094d4e6f976725c8
dmotte <37443982+dmotte@users.noreply.github.com>
2025-10-12 14:51:34 +0200

    Verbose fake-notifier-status.txt removal in test

--- test/main.sh
+++ test/main.sh
@@ -4,7 +4,7 @@ set -e
 
 cd "$(dirname "$0")"
 
-rm -f fake-notifier-status.txt
+rm -fv fake-notifier-status.txt
 
 bash fake-tail.sh |
     cargo run -q -- -di1 -m10 -- bash fake-notifier.sh 2>&1 |
